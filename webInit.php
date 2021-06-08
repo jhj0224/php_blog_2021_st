0ef72f7e2a145b6d03a6637909b5da08d6192d74
jhj0224 <jhj8664@gmail.com>
2021-06-08 23:13:48 +0900

    내 서버 연결

--- webInit.php
+++ webInit.php
@@ -4,3 +4,5 @@ require_once __DIR__ . "/util.php";
 require_once __DIR__ . "/app.php";
 
 $dbConn = mysqli_connect("127.0.0.1", "st__2021_04_full__site12", "1234", "st__2021_04_full__site12") or die("DB CONNECTION ERROR");
+
+?>
